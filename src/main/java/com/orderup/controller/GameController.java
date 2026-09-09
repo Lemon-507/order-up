ff2e39d78217a2a21ac948b27677c6ef62a4c5f3
wei <w18534752886@126.com>
2026-09-09 17:04:41 +0800

    fix:解决游戏界面不能正常运行的bug

--- src/main/java/com/orderup/controller/GameController.java
+++ src/main/java/com/orderup/controller/GameController.java
@@ -6,9 +6,9 @@ import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
-import javafx.scene.cntrol.Label;
 
 import com.orderup.util.GameTimer;
 
